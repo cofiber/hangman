44c4d52cb4479884771c7ae255d89a8773e9830a
cofiber <chen20131107@163.com>
2021-04-01 15:12:35 +0800

    my fist commit

---
+++ try11.py
@@ -0,0 +1,10 @@
+a=[1,55,2,99,54,7,63,4,3,7,9,14]
+import statistics
+a1=statistics.mean(a)
+print(a1)
+a2=statistics.median(a)
+print(a2)
+a3=statistics.mode(a)
+print(a3)
+import cubed
+cubed.x3()
